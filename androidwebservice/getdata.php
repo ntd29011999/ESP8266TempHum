6b6b54a048ec72c05dfa15c929340daf0baca714
duong <ntd29011999@gmail.com>
2021-04-14 02:09:13 +0700

    get update

--- androidwebservice/getdata.php
+++ androidwebservice/getdata.php
@@ -8,18 +8,20 @@
 
     //du lieu
 class moitruong{
-    function moitruong ($id,$nhietdo, $doam, $mota){
+    function moitruong ($id,$nhietdo, $doam, $mota, $Ngay, $Thoigian){
         $this->id = $id;
         $this->nhietdo = $nhietdo;
         $this->doam = $doam;
         $this->mota = $mota;
+        $this->Ngay = $Ngay;
+        $this->Thoigian = $Thoigian;
     }
 }
 // tao mang
 $mangmoitruong = array();
 // them mang 
 while($row = mysqli_fetch_assoc($data)){
-    array_push($mangmoitruong, new moitruong($row['id'],$row['nhietdo'],$row['doam'],$row['mota']));
+    array_push($mangmoitruong, new moitruong($row['id'],$row['nhietdo'],$row['doam'],$row['mota'],$row['Ngay'],$row['Thoigian']));
 }
 echo json_encode($mangmoitruong);
 ?>
