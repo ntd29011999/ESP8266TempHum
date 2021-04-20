08649a13075693ab6807268182caeecbd2abe337
duong <ntd29011999@gmail.com>
2021-04-21 04:34:00 +0700

    them host va chinh sua wifi co the config

--- codeESP8266/TempHum/TempHum.ino
+++ codeESP8266/TempHum/TempHum.ino
@@ -9,171 +9,91 @@
   Note that this sketch uses LED_BUILTIN to find the pin with the internal LED
 */
 #include "DHT.h"
+#include <WiFiManager.h>
 #include <ESP8266WiFi.h>                // Thư viện dùng để kết nối WiFi của ESP8266
 #include <ESP8266HTTPClient.h>
 #include <Arduino.h>
 #include <IRremoteESP8266.h>
-#include <IRrecv.h>
+#include <IRsend.h>
+#include <ir_Daikin.h>
 #include <IRutils.h>
 #include "BaseOTA.h"
 #include <assert.h>
-#include <IRac.h>
-#include <IRtext.h>
 
 
+
+// DHT11 Nhiet do
 #define DHTPIN 5 // what digital pin we're connected to
 #define DHTTYPE DHT11 // DHT 11
 
+// Hong ngoai may lanh
+const uint16_t kIrLed = 4;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
+IRDaikinESP ac(kIrLed);  // Set the GPIO to be used to sending the message
 
-
-// ==================== start of TUNEABLE PARAMETERS ====================
-// An IR detector/demodulator is connected to GPIO pin 14
-// e.g. D5 on a NodeMCU board.
-// Note: GPIO 16 won't work on the ESP8266 as it does not have interrupts.
-const uint16_t kRecvPin = 4;
-
-// The Serial connection baud rate.
-// i.e. Status message will be sent to the PC at this baud rate.
-// Try to avoid slow speeds like 9600, as you will miss messages and
-// cause other problems. 115200 (or faster) is recommended.
-// NOTE: Make sure you set your Serial Monitor to the same speed.
-const uint32_t kBaudRate = 115200;
-
-// As this program is a special purpose capture/decoder, let us use a larger
-// than normal buffer so we can handle Air Conditioner remote codes.
-const uint16_t kCaptureBufferSize = 1024;
-
-// kTimeout is the Nr. of milli-Seconds of no-more-data before we consider a
-// message ended.
-// This parameter is an interesting trade-off. The longer the timeout, the more
-// complex a message it can capture. e.g. Some device protocols will send
-// multiple message packets in quick succession, like Air Conditioner remotes.
-// Air Coniditioner protocols often have a considerable gap (20-40+ms) between
-// packets.
-// The downside of a large timeout value is a lot of less complex protocols
-// send multiple messages when the remote's button is held down. The gap between
-// them is often also around 20+ms. This can result in the raw data be 2-3+
-// times larger than needed as it has captured 2-3+ messages in a single
-// capture. Setting a low timeout value can resolve this.
-// So, choosing the best kTimeout value for your use particular case is
-// quite nuanced. Good luck and happy hunting.
-// NOTE: Don't exceed kMaxTimeoutMs. Typically 130ms.
-#if DECODE_AC
-// Some A/C units have gaps in their protocols of ~40ms. e.g. Kelvinator
-// A value this large may swallow repeats of some protocols
-const uint8_t kTimeout = 50;
-#else   // DECODE_AC
-// Suits most messages, while not swallowing many repeats.
-const uint8_t kTimeout = 15;
-#endif  // DECODE_AC
-// Alternatives:
-// const uint8_t kTimeout = 90;
-// Suits messages with big gaps like XMP-1 & some aircon units, but can
-// accidentally swallow repeated messages in the rawData[] output.
-//
-// const uint8_t kTimeout = kMaxTimeoutMs;
-// This will set it to our currently allowed maximum.
-// Values this high are problematic because it is roughly the typical boundary
-// where most messages repeat.
-// e.g. It will stop decoding a message and start sending it to serial at
-//      precisely the time when the next message is likely to be transmitted,
-//      and may miss it.
-
-// Set the smallest sized "UNKNOWN" message packets we actually care about.
-// This value helps reduce the false-positive detection rate of IR background
-// noise as real messages. The chances of background IR noise getting detected
-// as a message increases with the length of the kTimeout value. (See above)
-// The downside of setting this message too large is you can miss some valid
-// short messages for protocols that this library doesn't yet decode.
-//
-// Set higher if you get lots of random short UNKNOWN messages when nothing
-// should be sending a message.
-// Set lower if you are sure your setup is working, but it doesn't see messages
-// from your device. (e.g. Other IR remotes work.)
-// NOTE: Set this value very high to effectively turn off UNKNOWN detection.
-const uint16_t kMinUnknownSize = 12;
-
-// How much percentage lee way do we give to incoming signals in order to match
-// it?
-// e.g. +/- 25% (default) to an expected value of 500 would mean matching a
-//      value between 375 & 625 inclusive.
-// Note: Default is 25(%). Going to a value >= 50(%) will cause some protocols
-//       to no longer match correctly. In normal situations you probably do not
-//       need to adjust this value. Typically that's when the library detects
-//       your remote's message some of the time, but not all of the time.
-const uint8_t kTolerancePercentage = kTolerance;  // kTolerance is normally 25%
-
-// Legacy (No longer supported!)
-//
-// Change to `true` if you miss/need the old "Raw Timing[]" display.
-#define LEGACY_TIMING_INFO false
-// ==================== end of TUNEABLE PARAMETERS ====================
-
-// Use turn on the save buffer feature for more complete capture coverage.
-IRrecv irrecv(kRecvPin, kCaptureBufferSize, kTimeout, true);
-decode_results results;  // Somewhere to store the results
 
 
 // WIFI
-const char* ssid = "THANH TRUNG";         // Tên của mạng WiFi mà bạn muốn kết nối đến
-const char* password = "lalala12";// Mật khẩu của mạng WiFi
-
+//const char* ssid = "THANH TRUNG";         // Tên của mạng WiFi mà bạn muốn kết nối đến
+//const char* password = "lalala12";// Mật khẩu của mạng WiFi
+int value_off = 0;
 
 String nhietdo, doam, mota, postData; // value for POSTDATA 
 
 // SENSOR nhiet do
 DHT dht(DHTPIN, DHTTYPE); // sensor temp, hum
 
-void setup() {  
+void setup() {
+  ac.begin();  
+  // baudrate
   Serial.begin(115200);
+  // LED
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
 
   
-  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
-  Serial.print("Hello World");
-  
   dht.begin(); // Start dht
-  
-  WiFi.begin(ssid, password);           // Kết nối vào mạng WiFi
-  Serial.print("Connecting to ");
-  Serial.print(ssid);
-  // Chờ kết nối WiFi được thiết lập
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
-  }
+ WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    
+    // WiFi.mode(WiFi_STA); // it is a good practice to make sure your code sets wifi mode how you want it.
+
+    //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wm;
+
+    //reset settings - wipe credentials for testing
+    //wm.resetSettings();
+
+    // Automatically connect using saved credentials,
+    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
+    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
+    // then goes into a blocking loop awaiting configuration and will return success result
+
+    bool res;
+    // res = wm.autoConnect(); // auto generated AP name from chipid
+    // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
+    res = wm.autoConnect("AutoConnectAP","password"); // password protected ap
+
+    if(!res) {
+        Serial.println("Failed to connect");
+        // ESP.restart();
+    } 
+    else {
+        //if you get here you have connected to the WiFi    
+        Serial.println("connected...yeey :)");
+    }
+
   Serial.println("\n");
   Serial.println("Connection established!");  
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());       // Gởi địa chỉ IP đến máy tinh
 
-  //cau hinh IRreceive
-  OTAwifi();  // start default wifi (previously saved on the ESP) for OTA
-#if defined(ESP8266)
-  Serial.begin(kBaudRate, SERIAL_8N1, SERIAL_TX_ONLY);
-#else  // ESP8266
-  Serial.begin(kBaudRate, SERIAL_8N1);
-#endif  // ESP8266
-  while (!Serial)  // Wait for the serial connection to be establised.
-    delay(50);
-  // Perform a low level sanity checks that the compiler performs bit field
-  // packing as we expect and Endianness is as we expect.
-  assert(irutils::lowLevelSanityCheck() == 0);
-
-  Serial.printf("\n" D_STR_IRRECVDUMP_STARTUP "\n", kRecvPin);
-  OTAinit();  // setup OTA handlers and show IP
-#if DECODE_HASH
-  // Ignore messages with less than minimum on or off pulses.
-  irrecv.setUnknownThreshold(kMinUnknownSize);
-#endif  // DECODE_HASH
-  irrecv.setTolerance(kTolerancePercentage);  // Override the default tolerance.
-  irrecv.enableIRIn();  // Start the receiver
-  //-------END -------------
 }
 
 // the loop function runs over and over again forever
 void loop() {
    // Wait a few seconds between measurements.
-      delay(5000);
+      
       
       HTTPClient http; 
 // Reading temperature or humidity takes about 250 milliseconds!
@@ -195,14 +115,55 @@ void loop() {
 //convert value to String
       nhietdo = String(t);
       doam = String(h);
-      if (t >= 23 && t <= 28)
+      if (t >= 23 && t <= 28){
         mota = "binh thuong";
-      else if (t > 28)
+       
+        if(t <=26.5){
+          if (value_off != 0 && value_off < 3) value_off = 3;
+          if (value_off<7 && value_off >= 3){
+          ac.off();
+          #if SEND_DAIKIN
+          ac.send();
+          value_off++;
+          if (value_off == 6)
+            value_off = 0;
+          #endif  // SEND_DAIKIN
+          
+          }
+        }
+      }
+      else if (t > 28){
         mota = "nong";
+        if(value_off != 4 && value_off > 3) value_off = 0;
+        if(value_off < 3){
+          
+          Serial.println("Sending...");
+
+          // Set up what we want to send. See ir_Daikin.cpp for all the options.
+          ac.on();
+          ac.setFan(5);
+          ac.setMode(kDaikinCool);
+          ac.setTemp(25);
+          ac.setSwingVertical(false);
+          ac.setSwingHorizontal(false);
+        
+          // Set the current time to 1:33PM (13:33)
+          // Time works in minutes past midnight
+
+          // Display what we are going to send.
+          Serial.println(ac.toString());
+        
+          // Now send the IR signal.
+        #if SEND_DAIKIN
+          ac.send();
+        #endif  // SEND_DAIKIN
+        value_off++;
+        }
+      }
       else mota = "lanh";
       postData = "nhietdo=" + nhietdo + "&doam=" + doam + "&mota=" + mota;
 
-      http.begin("http://192.168.1.3/androidwebservice/insertdata.php");              // Connect to host where MySQL databse is hosted
+      http.begin("http://ntd29011999.000webhostapp.com/insertdata.php");              // Connect to host where MySQL databse is hosted
       http.addHeader("Content-Type", "application/x-www-form-urlencoded");            //Specify content-type header
  
       int httpCode = http.POST(postData);   // Send POST request to php file and store server response code in variable named httpCode
@@ -225,38 +186,11 @@ void loop() {
         http.end(); 
         return; 
         }
-        delay(2000);
+        Serial.println();
+        Serial.println(value_off);
+         Serial.println();
+    delay(900000);
 
-        
-     if (irrecv.decode(&results)) {
-    // Display a crude timestamp.
-    uint32_t now = millis();
-    Serial.printf(D_STR_TIMESTAMP " : %06u.%03u\n", now / 1000, now % 1000);
-    // Check if we got an IR message that was to big for our capture buffer.
-    if (results.overflow)
-      Serial.printf(D_WARN_BUFFERFULL "\n", kCaptureBufferSize);
-    // Display the library version the message was captured with.
-    Serial.println(D_STR_LIBRARY "   : v" _IRREMOTEESP8266_VERSION_ "\n");
-    // Display the tolerance percentage if it has been change from the default.
-    if (kTolerancePercentage != kTolerance)
-      Serial.printf(D_STR_TOLERANCE " : %d%%\n", kTolerancePercentage);
-    // Display the basic output of what we found.
-    Serial.print(resultToHumanReadableBasic(&results));
-    // Display any extra A/C info if we have it.
-    String description = IRAcUtils::resultAcToString(&results);
-    if (description.length()) Serial.println(D_STR_MESGDESC ": " + description);
-    yield();  // Feed the WDT as the text output can take a while to print.
-#if LEGACY_TIMING_INFO
-    // Output legacy RAW timing info of the result.
-    Serial.println(resultToTimingInfo(&results));
-    yield();  // Feed the WDT (again)
-#endif  // LEGACY_TIMING_INFO
-    // Output the results as source code
-    Serial.println(resultToSourceCode(&results));
-    Serial.println();    // Blank line between entries
-    yield();             // Feed the WDT (again)
-  }
-  OTAloopHandler();
 }
 
 
